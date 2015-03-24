a72a00eb93cb93ef5ac95aa0e435f6cc43bb886c
Harris Tzovanakis <me@drjova.com>
2015-03-24 09:43:03 +0100

    multimedia: module deprecation
    * INCOMPATIBLE Replaces multimedia module with Flask-IIIF extension.
    
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- invenio/modules/documents/utils.py
+++ invenio/modules/documents/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2014 CERN.
+# Copyright (C) 2015 CERN.
 #
 # Invenio is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -17,4 +17,16 @@
 # along with Invenio; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Test Multimedia API."""
+"""Document utils."""
+
+
+def uuid_to_path(uuid):
+    """Convert `~invenio.modules.documents.api.Document` to path."""
+    from invenio.modules.documents.api import Document
+
+    try:
+        document = Document.get_document(uuid)
+    except Exception:
+        raise
+    else:
+        return document.get('uri', '')
